5d719b8be06a877c8f38df745f73741c7a9522ff
dx07 <dx.msr07@gmail.com>
2018-11-25 18:55:40 +0100

    New Dataset + Début rapport

--- Projet/projet.r
+++ Projet/projet.r
@@ -1,6 +1,10 @@
-commerce = read.csv("~/Documents/TMDS/womenclothingecommercereviews.csv",sep=";")
+# https://www.kaggle.com/nicapotato/womens-ecommerce-clothing-reviews
+
+vgsales = read.csv("~/../PycharmProjects/5SDBD-R/Projet/vgsales.csv",sep=",")
 
 library(ggplot2)
 library(plyr)
 library(stringr)
+
+head(vgsales)
 
--- TP/exo2.r
+++ TP/exo2.r
@@ -2,60 +2,60 @@
 
 # ---------------- 1 ---------------- #
 
-prenoms = read.csv("~/Documents/TMDS/prenoms.csv",sep=";")
+prenoms = read.csv("~/../PycharmProjects/5SDBD-R/TP/prenoms.csv",sep=";")
 
 library(ggplot2)
 library(plyr)
 library(stringr)
 
-#prenSex = ddply(prenoms, c("Ann√©e"), function(x){sum(x$Nombre)})
-prenSex = ddply(prenoms, c("Ann√©e"), summarise,Total=sum(Nombre))
-ggplot(prenSex,aes(factor(Ann√©e),Total)) + geom_bar(stat="identity",position="dodge") + xlab("Year") + ylab("Number")
+#prenSex = ddply(prenoms, c("Ann√.e"), function(x){sum(x$Nombre)})
+prenSex = ddply(prenoms, c("Ann√.e"), summarise,Total=sum(Nombre))
+ggplot(prenSex,aes(factor(Ann√.e),Total)) + geom_bar(stat="identity",position="dodge") + xlab("Year") + ylab("Number")
 
-prenSex = ddply(prenoms, c("Ann√©e","Sexe"), function(x){sum(x$Nombre)})
-ggplot(prenSex,aes(factor(Ann√©e),V1,fill=Sexe)) + geom_bar(stat="identity",position="dodge") + xlab("Year") + ylab("Number")
+prenSex = ddply(prenoms, c("Ann√.e","Sexe"), function(x){sum(x$Nombre)})
+ggplot(prenSex,aes(factor(Ann√.e),V1,fill=Sexe)) + geom_bar(stat="identity",position="dodge") + xlab("Year") + ylab("Number")
 
-"Jean-C√©lestin" %in% prenoms$Pr√©nom
+"Jean-CÈlestin" %in% prenoms$Pr√.nom
 
-prenSex = ddply(prenoms, c("Pr√©nom","Sexe"), function(x){sum(x$Nombre)})
+prenSex = ddply(prenoms, c("Pr√.nom","Sexe"), function(x){sum(x$Nombre)})
 prenSex[order(-(prenSex$V1)),][1:10,]
 
-prenTop = ddply(prenoms, c("Ann√©e","Sexe","Pr√©nom"), function(x){sum(x$Nombre)})
-prenTop = prenSex[order(prenSex$Ann√©e,prenSex$Sexe,(-(prenSex$V1))),]
+prenTop = ddply(prenoms, c("Ann√.e","Sexe","Pr√.nom"), function(x){sum(x$Nombre)})
+prenTop = prenSex[order(prenSex$Ann√.e,prenSex$Sexe,(-(prenSex$V1))),]
 prenTopF = subset(prenTop,Sexe == 'F')
 prenTopM = subset(prenTop,Sexe == 'M')
-prenTop5F = subset(prenTopF,Pr√©nom %in% Pr√©nom[1:5])
-prenTop5M = subset(prenTopM,Pr√©nom %in% Pr√©nom[1:5])
-ggplot(data=prenTop5F, aes(x=Ann√©e, y=V1, colour=Pr√©nom)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Popularity of Top 5 female names since 2003") + xlab("Ann√©es") + ylab("Nombre de naissances") + scale_x_continuous(breaks = seq(2003, 2012))
-ggplot(data=prenTop5M, aes(x=Ann√©e, y=V1, colour=Pr√©nom)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Popularity of Top 5 male names since 2003") + xlab("Ann√©es") + ylab("Nombre de naissances") + scale_x_continuous(breaks = seq(2003, 2012))
+prenTop5F = subset(prenTopF,Pr√.nom %in% Pr√.nom[1:5])
+prenTop5M = subset(prenTopM,Pr√.nom %in% Pr√.nom[1:5])
+ggplot(data=prenTop5F, aes(x=Ann√.e, y=V1, colour=Pr√.nom)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Popularity of Top 5 female names since 2003") + xlab("Ann√.es") + ylab("Nombre de naissances") + scale_x_continuous(breaks = seq(2003, 2012))
+ggplot(data=prenTop5M, aes(x=Ann√.e, y=V1, colour=Pr√.nom)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Popularity of Top 5 male names since 2003") + xlab("Ann√.es") + ylab("Nombre de naissances") + scale_x_continuous(breaks = seq(2003, 2012))
 
-prenLen = ddply(prenoms, c("Ann√©e","Sexe","Pr√©nom"), summarise,Taille=nchar(toString(Pr√©nom)))
-prenLen = ddply(prenLen, c("Ann√©e"),summarise,Moyenne=mean(Taille))
-ggplot(data=prenLen, aes(x=Ann√©e, y=Moyenne)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Taille moyenne des pr√©noms par ann√©e") + xlab("Ann√©es") + ylab("Nombre de lettres") + scale_x_continuous(breaks = seq(2003, 2012))
+prenLen = ddply(prenoms, c("Ann√.e","Sexe","Pr√.nom"), summarise,Taille=nchar(toString(Pr√.nom)))
+prenLen = ddply(prenLen, c("Ann√.e"),summarise,Moyenne=mean(Taille))
+ggplot(data=prenLen, aes(x=Ann√.e, y=Moyenne)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Taille moyenne des Pr√.noms par Ann√.e") + xlab("Ann√.es") + ylab("Nombre de lettres") + scale_x_continuous(breaks = seq(2003, 2012))
 
 a = 
 a[a %in% vowels]
 a[a %in% setdiff(letters,vowels)]
 
 prenLen = ddply(prenoms, 
-                   c("Ann√©e","Sexe","Pr√©nom"), 
+                   c("Ann√.e","Sexe","Pr√.nom"), 
                    summarise,
-                   Splitted = strsplit(str_to_lower(iconv(toString(Pr√©nom),to="ASCII//TRANSLIT")),""),
+                   Splitted = strsplit(str_to_lower(iconv(toString(Pr√.nom),to="ASCII//TRANSLIT")),""),
                    Vowels = length(Splitted[[1]][Splitted[[1]] %in% vowels]),
                    Consonants = length(Splitted[[1]][Splitted[[1]] %in% setdiff(letters,vowels)])
 )
-prenLen = ddply(prenLen, c("Ann√©e"),summarise,MoyenneV=mean(Vowels),MoyenneC=mean(Consonants))
-ggplot(data=prenLen, aes(x=Ann√©e, y=MoyenneC)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre moyen de consonnes par ann√©e") + xlab("Ann√©es") + ylab("Nombre de consonnes") + scale_x_continuous(breaks = seq(2003, 2012))
-ggplot(data=prenLen, aes(x=Ann√©e, y=MoyenneV)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre moyen de voyelles par ann√©e") + xlab("Ann√©es") + ylab("Nombre de voyelles") + scale_x_continuous(breaks = seq(2003, 2012))
+prenLen = ddply(prenLen, c("Ann√.e"),summarise,MoyenneV=mean(Vowels),MoyenneC=mean(Consonants))
+ggplot(data=prenLen, aes(x=Ann√.e, y=MoyenneC)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre moyen de consonnes par Ann√.e") + xlab("Ann√.es") + ylab("Nombre de consonnes") + scale_x_continuous(breaks = seq(2003, 2012))
+ggplot(data=prenLen, aes(x=Ann√.e, y=MoyenneV)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre moyen de voyelles par Ann√.e") + xlab("Ann√.es") + ylab("Nombre de voyelles") + scale_x_continuous(breaks = seq(2003, 2012))
 
 prenComp = ddply(prenoms, 
-                c("Ann√©e","Sexe","Nombre","Pr√©nom"), 
+                c("Ann√.e","Sexe","Nombre","Pr√.nom"), 
                 summarise,
-                Splitted = strsplit(toString(Pr√©nom),""),
+                Splitted = strsplit(toString(Pr√.nom),""),
                 Composed = '-' %in% Splitted[[1]]
 )
-prenComp = ddply(prenComp, c("Ann√©e"),summarise,MoyenneCompos√©s=sum(Composed*Nombre))
-ggplot(data=prenComp, aes(x=Ann√©e, y=MoyenneCompos√©s)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre de pr√©noms compos√©s par ann√©e") + xlab("Ann√©es") + ylab("Nombre de pr√©noms compos√©s") + scale_x_continuous(breaks = seq(2003, 2012))
+prenComp = ddply(prenComp, c("Ann√.e"),summarise,MoyenneComposÈs=sum(Composed*Nombre))
+ggplot(data=prenComp, aes(x=Ann√.e, y=MoyenneComposÈs)) + geom_point(size=2) + geom_line(size=1) + ggtitle("Nombre de Pr√.noms composÈs par Ann√.e") + xlab("Ann√.es") + ylab("Nombre de Pr√.noms composÈs") + scale_x_continuous(breaks = seq(2003, 2012))
 
 
 
